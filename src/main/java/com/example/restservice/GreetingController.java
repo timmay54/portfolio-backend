194d010f685f4610d50d6034710f397ece5515ee
timmay54 <timothymardesen@yahoo.com>
2020-11-22 18:01:41 -0600

    added springboot REST

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,38 @@
+package com.example.restservice;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@CrossOrigin /*(origins = "http://localhost:4200/", allowedHeaders = "*")*/
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	 
+	
+	@RequestMapping(value="/process", method = RequestMethod.POST)
+    public void process(@RequestBody String payload) throws Exception {
+        System.out.println(payload);
+    }
+
+}
